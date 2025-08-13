e315cb0ced6d9368dccba54ae0a2a705072727f0
yugandhargupta <yugandhar85work@gmail.com>
2025-08-13 11:36:45 +0530

    reports add

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,24 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'https://practice.expandtesting.com',
+    baseUrl: 'https://practice.expandtesting.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Register cypress-mochawesome-reporter plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // default spec location
+    supportFile: 'cypress/support/e2e.js'
   },
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",  // Matches your GitHub Actions artifact path
+    charts: true,
+    reportPageTitle: "Cypress Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: false,
+    html: true,
+    json: true
+  }
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,5 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+import 'cypress-mochawesome-reporter/register';
